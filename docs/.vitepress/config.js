0274538549ea0c5450c2b3de8c8b92c6d9c52256
luoway <luoway@outlook.com>
2023-08-17 16:53:09 +0800

    add @vite-pwa/vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vitepress'
+import { withPwa } from '@vite-pwa/vitepress'
+
 import githubLink from './github-link'
 import sidebar from './sidebar'
 console.log('sidebar: ', sidebar)
 
-export default defineConfig({
+export default withPwa(defineConfig({
     title: '前端路线图',
+    description: '前端路线图，记录个人积累',
+    lang: 'zh-CN',
     themeConfig: {
         socialLinks: [
           { icon: 'github', link: githubLink },
@@ -24,4 +28,40 @@ export default defineConfig({
     markdown: {
         theme: 'github-dark',
     },
-})
+    head: [
+        [
+            'link',
+            {
+                rel: 'apple-touch-icon',
+                href: '/apple-touch-icon.png',
+                sizes: '180x180'
+            }
+        ]
+    ],
+    paw: {
+        registerType: 'autoUpdate',
+        includeAssets: [
+            'favicon.icon',
+            'robots.txt',
+            'apple-touch-icon.jpeg',
+        ],
+        manifest: {
+            id: 'frontend-road-map',
+            name: '前端路线图',
+            short_name: '前端路线图',
+            description: '前端路线图，记录个人积累',
+            theme_color: '#80B40D',
+            background_color: '#80B40D',
+            icons: [
+                {
+                    src: '512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ]
+        },
+        workbox: {
+            globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
+        },
+    }
+}))
